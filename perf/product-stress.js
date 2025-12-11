cff5393aa6d39f4dcfd6bc38daad21ea24090d3a
nhieelam <lam.nhiee11@gmail.com>
2025-12-11 20:04:18 +0700

    up

--- perf/product-stress.js
+++
@@ -1,6 +0,0 @@
-import http from 'k6/http';
-import { check, sleep } from 'k6';
-
-const BASE_URL = 'http://localhost:8080/api'; // or your backend
-const USERNAME = 'lam123';
-const PASSWORD = 'Password123';
